064b89f63eead1609a29f8b10dddd453f13f7326
Ometek <kuba.ornatek@gmail.com>
2024-07-09 13:35:47 +0200

    Started the project.

--- 001_password_manager/main.cpp
+++ 001_password_manager/main.cpp
@@ -1,13 +1,48 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 using namespace std;
 
-int main(){
-    cout << "Hello World!";
+int main() {
+    // wczytanie danych
+    ifstream wejscie;
+    wejscie.open("data.txt");
+
+    vector<pair<string, string>> dane;
+    string user, password;
+
+    while (wejscie >> user) {
+        wejscie >> password;
+        dane.push_back({user, password});
+    }
+
+    // obsługa poleceń
+    while (true) {
+        string polecenie;
+        cin >> polecenie;
+
+        if (polecenie == "exit")
+            break;
+
+        if (polecenie == "add") {
+            cin >> user >> password;
+            dane.push_back({user, password});
+        }
+
+        if (polecenie == "help") {
+            cin >> user;
+            // cout << password;
+        }
+
+        if (polecenie == "delete") {
+            cin >> user;
+        }
+    }
+    
+    // zapisanie do pliku
+    cout << "Zapisuje do pliku...\n";
+    for (auto [k, s] : dane) {
+        cout << k << " -> " << s << "\n";
+    }
 }
 
-/*
-
-c++ -o run main.cpp
-./run
-
-*/
